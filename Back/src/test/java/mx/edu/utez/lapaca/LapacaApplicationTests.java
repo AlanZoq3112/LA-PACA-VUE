8df8b21113811f34f0dd301824492e03d9a98685
eduardo-chan <20213tn001@utez.edu.mx>
2024-02-25 19:49:09 -0600

    [ADD] Implementación del Patrón de Arquitectura MVC (Modelo-Vista-Controlador)

---
+++ Back/src/test/java/mx/edu/utez/lapaca/LapacaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.lapaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

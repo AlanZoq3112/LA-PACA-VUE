161111bca158850101691ced1bfd6b46628966c4
MarcoRayo <20193tn022@utez.edu.mx>
2024-04-02 00:36:05 -0600

    [DELETE] login prueba

--- LoginJWT/src/main/java/mx/edu/edu/loginjwt/dto/RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package mx.edu.edu.loginjwt.dto;
-
-import lombok.Data;
-
-@Data
-public class RefreshTokenRequest {
-    private String token;
-
-}
--- LoginJWT/src/main/java/mx/edu/edu/loginjwt/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package mx.edu.edu.loginjwt.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
